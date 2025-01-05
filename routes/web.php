e911293a7396a24513a544431b217bb42e988d03
Sajad Rab <120600259+SajadAliarab@users.noreply.github.com>
2025-01-05 21:42:51 +0000

    change home button

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,5 @@ Route::prefix('custom')->group(function () {
     Route::get('/', function () {
         return view('filament-panels::pages.dashboard');
     });
-    Route::get('http://localhost:300', )->name('home');
+    Route::get('https://community.sajad.uk', )->name('home');
 });
